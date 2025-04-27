16efe33b9d4a72183c83da2c2c72295fc08500f2
g.balayan <g.balayan@vk.team>
2025-04-28 00:15:26 +0300

    add

--- app/rabbitmq_connection.py
+++ app/rabbitmq_connection.py
@@ -2,11 +2,14 @@ import aio_pika
 import os
 
 RABBITMQ_SETTINGS = {
-    "host": os.getenv("RABBITMQ_HOST", "rabbitmq"),
-    "port": int(os.getenv("RABBITMQ_PORT", 5672)),
-    "login": os.getenv("RABBITMQ_USER", "guest"),
-    "password": os.getenv("RABBITMQ_PASS", "guest"),
+    "host": os.getenv("RABBITMQ_HOST"),
+    "port": int(os.getenv("RABBITMQ_PORT")),
+    "login": os.getenv("RABBITMQ_USER"),
+    "password": os.getenv("RABBITMQ_PASS"),
 }
+EXCHANGE_NAME = os.getenv("EXCHANGE_NAME")
+QUEUE_NAME = os.getenv("QUEUE_NAME")
+
 async def get_connection():
     return await aio_pika.connect_robust(**RABBITMQ_SETTINGS)
 
@@ -15,3 +18,20 @@ async def get_channel():
     channel = await connection.channel()
     await channel.set_qos(prefetch_count=10)
     return channel
+
+async def setup_rabbitmq(channel):
+    # Объявляем Exchange (fanout)
+    exchange = await channel.declare_exchange(
+        EXCHANGE_NAME,
+        aio_pika.ExchangeType.FANOUT,
+        durable=True
+    )
+
+    # Объявляем очередь
+    queue = await channel.declare_queue(
+        QUEUE_NAME,
+        durable=True
+    )
+    # Привязываем очередь к exchange по имени
+    await queue.bind(exchange.name)
+    return queue
